206b97557696d32bd299bb71b0cb2fe0808203e3
NicholasDollick <nidollick@gmail.com>
2019-05-19 14:21:58 -0700

    update to message return model to properly show usernames

--- QNTM.API/Dtos/MessageToReturnDto.cs
+++ QNTM.API/Dtos/MessageToReturnDto.cs
@@ -6,9 +6,9 @@ namespace QNTM.API.Dtos
     {
         public int Id { get; set; }
         public int SenderId { get; set; }
-        public string SenderKnownAs { get; set; }
+        public string SenderUsername { get; set; }
         public int RecipientId { get; set; }
-        public string RecipientKnownAs { get; set; }
+        public string RecipientUsername { get; set; }
         public string Content { get; set; }
         public bool IsRead { get; set; }
         public DateTime? DateRead { get; set; }
