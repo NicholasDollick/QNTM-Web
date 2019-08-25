febb6d72db2b19f01ae3effaad9da5f008c88e2e
NicholasDollick <nidollick@gmail.com>
2019-08-25 12:31:12 -0700

    added ignores

--- QNTM.API/Hubs/ChatHub.cs
+++ QNTM.API/Hubs/ChatHub.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 using QNTM.API.Data;
@@ -20,6 +21,37 @@ namespace QNTM.API.Hubs
         public void ChatMessages(string jsonData)
         {
             Clients.All.SendAsync("chatMessages", jsonData);
+        }
+
+        // sends to single user
+        // based on CliamTypes.NameIdentifier
+        public Task SendPrivateMessage(string user, string message)
+        {
+            Console.WriteLine(user);
+            Console.WriteLine(Context.User.Identity);
+            Console.WriteLine(Context.ConnectionId);
+            // Console.WriteLine(ConnectedUse);
+            return Clients.User(user).SendAsync("RecievedMessage", message);
+        }
+
+        // Code for "groups"
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}");
+        }
+
+        public Task SendMessageToGroup(string group, string message)
+        {
+            return Clients.Group(group).SendAsync("RecievedMessage", message);
         }
     }
 }
