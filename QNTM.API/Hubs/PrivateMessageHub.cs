1cb4db69c2499c3ed38965e04217547f83d6b16a
NicholasDollick <nidollick@gmail.com>
2019-09-04 21:18:34 -0700

    expanded PmHub for testing

--- QNTM.API/Hubs/PrivateMessageHub.cs
+++ QNTM.API/Hubs/PrivateMessageHub.cs
@@ -1,9 +1,49 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using QNTM.API.Helpers;
 
 namespace QNTM.API.Hubs
 {
     public class PrivateMessageHub : Hub
     {
-        
+        private UserHandler _userHandler;   
+
+        public PrivateMessageHub(UserHandler userHandler)
+        {
+            _userHandler = userHandler;
+        }
+
+        public async Task Leave()
+        {
+            _userHandler.RemoveFromDict(Context.User.Identity.Name);
+            
+            await Clients.AllExcept(new List<string> { Context.ConnectionId }).SendAsync("Disconnected", Context.User.Identity.Name);
+        }
+
+        public async Task Join()
+        {
+            /*
+            * if (new user)
+            * else (return user)
+             */
+            if (!_userHandler.UpdateDict(Context.User.Identity.Name, Context.ConnectionId))
+            {
+                var list = _userHandler.GetAllOtherUsers(Context.User.Identity.Name).ToList();
+                await Clients.AllExcept(new List<string> { Context.ConnectionId }).SendAsync("Now Online", Context.User.Identity.Name);
+            } else {}
+
+            await Clients.Client(Context.ConnectionId).SendAsync("Joined", _userHandler.GetUserData(Context.User.Identity.Name));
+            
+            await Clients.Client(Context.ConnectionId).SendAsync("OnlineUsers", _userHandler.GetAllOtherUsers(Context.User.Identity.Name));
+        }
+
+        public Task SendDirectMessage(string toUser, string message)
+        {
+            var userFrom = _userHandler.GetUserData(Context.User.Identity.Name);
+            var sendToUser = _userHandler.GetUserData(toUser);
+            return Clients.Client(sendToUser.ConnectionId).SendAsync("SendPm", message, userFrom);
+        }
     }
 }
