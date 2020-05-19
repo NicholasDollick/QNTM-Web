249d4d4df17b0502bb9e000fa9a3e84e19e8534c
NicholasDollick <nidollick@gmail.com>
2020-05-19 16:43:14 -0700

    starting on state idea

--- QNTM.API/Hubs/PrivateMessageHub.cs
+++ QNTM.API/Hubs/PrivateMessageHub.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 using QNTM.API.Data;
+using QNTM.API.Models;
 
 namespace QNTM.API.Hubs
 {
     [Authorize(AuthenticationSchemes = "Bearer")]
     public class PrivateMessageHub : Hub
     {
-        private IUserHandler _userHandler;   
+        private IUserHandler _userHandler;
+        private readonly IQNTMRepository _repo;
+        private readonly IMapper _mapper;
 
-        public PrivateMessageHub(IUserHandler userHandler)
+        public PrivateMessageHub(IUserHandler userHandler, IQNTMRepository repo, IMapper mapper)
         {
             _userHandler = userHandler;
+            _repo = repo;
+            _mapper = mapper;
         }
 
         public async Task Leave()
@@ -52,13 +58,31 @@ namespace QNTM.API.Hubs
             await Clients.Client(Context.ConnectionId).SendAsync("OnlineUsers", _userHandler.GetAllOtherUsers(Context.User.Identity.Name));
         }
 
+        // routes the message to the correct location
+        // adds the message to the db for both users
         public Task SendDirectMessage(string toUser, string message)
         {
             Console.WriteLine(Context.User.Identity.Name);
             var userFrom = _userHandler.GetUserData(Context.User.Identity.Name);
             var sendToUser = _userHandler.GetUserData(toUser);
-            Console.WriteLine(sendToUser.Username);
-            Console.WriteLine(sendToUser.ConnectionId);
+            
+
+            //TODO: the function fails while attempting to create this message object
+            var messageToCreate = new Message { 
+                SenderId = _repo.GetUser(Context.User.Identity.Name).Id,
+                RecipientId = _repo.GetUser(toUser).Id, Content = message, MessageSent = DateTime.Now 
+                };
+
+            Console.WriteLine(messageToCreate);
+
+            var messageToSave = _mapper.Map<Message>(messageToCreate);
+            Console.WriteLine(messageToSave);
+
+            _repo.Add(messageToSave);
+
+            if(!(_repo.SaveAll().Result))
+                throw new Exception("Message Failed To Save");
+
             return Clients.Client(sendToUser.ConnectionId).SendAsync("SendPm", message, userFrom);
         }
     }
