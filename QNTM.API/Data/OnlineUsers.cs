e74dd6d6dba2ab658a0e1b99539eabdcfce6cb6c
NicholasDollick <nidollick@gmail.com>
2020-12-26 18:12:42 -0800

    expanding api for online tracking

--- QNTM.API/Data/OnlineUsers.cs
+++ QNTM.API/Data/OnlineUsers.cs
@@ -1,4 +1,7 @@
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using QNTM.API.Models;
 
 namespace QNTM.API.Data
@@ -9,5 +12,52 @@ namespace QNTM.API.Data
         private readonly ConcurrentDictionary<string, ChatUserData> _onlineUsers = new ConcurrentDictionary<string, ChatUserData>();
 
         public ConcurrentDictionary<string, ChatUserData> Users => _onlineUsers;
+
+        private static readonly Dictionary<string, List<string>> UsersOnline = 
+                new Dictionary<string, List<string>>();
+
+        public Task UserConnected(string username, string connectionID)
+        {
+            lock(UsersOnline)
+            {
+                if(UsersOnline.ContainsKey(username))
+                {
+                    UsersOnline[username].Add(connectionID);
+                }
+                else
+                {
+                    UsersOnline.Add(username, new List<string>{connectionID});
+                }
+            }
+            return Task.CompletedTask;
+        }
+
+        public Task UserDisconnected(string username, string connectionID)
+        {
+            lock(UsersOnline)
+            {
+                if(!UsersOnline.ContainsKey(username))
+                    return Task.CompletedTask;
+
+                UsersOnline[username].Remove(connectionID);
+                if(UsersOnline[username].Count == 0)
+                {
+                    UsersOnline.Remove(username);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task<string[]> GetOnlineUsers()
+        {
+            string[] onlineUsers;
+            lock(UsersOnline)
+            {
+                onlineUsers = UsersOnline.OrderBy(k => k.Key).Select(k => k.Key).ToArray();
+            }
+
+            return Task.FromResult(onlineUsers);
+        }
     }
 }
--- QNTM.API/Hubs/PresenceHub.cs
+++ QNTM.API/Hubs/PresenceHub.cs
@@ -2,19 +2,37 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using QNTM.API.Data;
 
 namespace QNTM.API.Hubs
 {
     [Authorize]
     public class PresenceHub : Hub
     {
+
+        private readonly OnlineUsers _users;
+
+        public PresenceHub(OnlineUsers users)
+        {
+            _users = users;
+        }
+
         public override async Task OnConnectedAsync()
         {
+            await _users.UserConnected(Context.User.Identity.Name, Context.ConnectionId);
             await Clients.Others.SendAsync("UserIsOnline", Context.User.Identity.Name);
+
+            var currentUsers = await _users.GetOnlineUsers();
+            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
         }
+
         public override async Task OnDisconnectedAsync(Exception exception)
         {
+            await _users.UserDisconnected(Context.User.Identity.Name, Context.ConnectionId);
             await Clients.Others.SendAsync("UserIsOffline", Context.User.Identity.Name);
+
+            var currentUsers = await _users.GetOnlineUsers();
+            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
 
             await base.OnDisconnectedAsync(exception);
         }
